143326e7c7178e482193fb82739c6280602a4784
Jinwoo Bae <tkqlzz@gmail.com>
2017-08-01 23:41:21 +0900

    트리의 지름 [bfs+트리] [코드정리안됨]

--- simple.py
+++ simple.py
@@ -2,35 +2,55 @@ import collections
 
 
 def bfs():
-    q = collections.deque([[0, 0]])
-    direct = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-    cnt = 0
+    q = collections.deque()
+
+    temp = set()
+    for i in range(n+1):
+        if terminal[i]:
+            dp[i].append(0)
+            temp.add(rev_tree[i][0][0])
+    for i in temp:
+        q.append(i)
+
+
     while q:
-        y, x = q.popleft()
-        cnt += 1
+        i = q.popleft()
+        is_next = False
+        for b, v in tree[i]:
+            if not dp[b]:
+                is_next = True
+                break
+            dp[i].append(max(dp[b]) + v)
+        if is_next:
+            q.append(i)
+            continue
 
-        for dy, dx in direct:
-            cy, cx = y + dy, x + dx
+        if rev_tree[i]:
+            if rev_tree[i][0][0] not in q:
+                q.append(rev_tree[i][0][0])
+            terminal[i] = True
 
-            if cy < 0 or cx < 0 or cy >= n or cx >= m:
-                continue
+    res = 0
+    for d in dp:
+        res = max(res, sum(sorted(d, reverse=True)[:2]))
+    print(res)
 
-            if arr[cy][cx] == '1':
-                if dp[cy][cx] > dp[y][x] + 1:
-                    dp[cy][cx] = dp[y][x] + 1
-                    q.append([cy, cx])
-            elif arr[cy][cx] == '0':
-                if dp[cy][cx] > dp[y][x]:
-                    dp[cy][cx] = dp[y][x]
-                    q.append([cy, cx])
+n = int(input())
+tree = [[] for _ in range(n+1)]
+rev_tree = [[] for _ in range(n+1)]
+dp = [[] for _ in range(n+1)]
+terminal = [False] + [True] * n
 
-m, n = map(int, input().split())
-arr = []
-dp = [[987654321] * m for _ in range(n)]
-
-for i in range(n):
-    arr.append(input())
-dp[0][0] = int(arr[0][0])
+for i in range(n-1):
+    a, b, v = map(int, input().split())
+    tree[a].append([b, v])
+    rev_tree[b].append([a, v])
+    terminal[a] = False
 
 bfs()
-print(dp[n-1][m-1])
+
+"""
+1/7 15/4 6/10
+12/0 26/19
+15/28
+"""
