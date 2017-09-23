21aef78c7e18a1168e31e1a88c99c1c9949e6c2f
Jinwoo Bae <tkqlzz@gmail.com>
2017-09-23 21:03:37 +0900

    1024 수열의 합 - 구현

--- 1024.py
+++ 1024.py
@@ -1,12 +1,11 @@
 n, l = map(int, input().split())
 
-res = [-1]
 for i in range(l, 101):
-    mid = n//i
-    s = mid - (i-1)//2
-    e = mid + (i+2)//2
-    if s >= 0 and n == sum(i for i in range(s, e)):
-        res = [i for i in range(s, e)]
-        break
+    m = n//i
+    s = m - (i-1)//2
+    e = m + (i+2)//2
+    if s >= 0 and n == (s+e-1)*i/2:
+        print(' '.join(map(str, [i for i in range(s, e)])))
+        exit(0)
 
-print(' '.join(map(str, res)))
+print(-1)
